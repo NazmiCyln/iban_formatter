9f05487e7144faa705f31895ea0904776ef06b04
Nazmi Ceylan <nazmican50@gmail.com>
2024-06-02 16:19:26 +0300

    initial commit

--- lib/src/regexp.dart
+++ lib/src/regexp.dart
@@ -1,5 +1,7 @@
-final specialAndAlphaCharacter =
-    RegExp(r"[a-zA-Z\$\R\Ü\Ö\T\Q\Ç\%\#\@\_\:\!\?\&\|\£\½\§\{\[\]\}\\\>\<\=\'\.\,\*\-\,+\ü\ğ\ö\ç\/\;\(\)]+$");
+final specialAndAlphaCharacter = RegExp(
+  r"[a-zA-Z\$\R\Ü\Ö\T\Q\Ç\%\#\@\_\:\!\?\&\|\£\½\§\{\[\]\}\\\>\<\=\'\.\,\*\-\,+\ü\ğ\ö\ç\/\;\(\)]+$",
+);
 
-final emoji =
-    RegExp(r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])');
+final emoji = RegExp(
+  r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])',
+);
